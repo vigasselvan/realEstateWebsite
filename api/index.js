247c4010a79069dc4e39b27008c28f095cf1cb66
vigas <vigas54209@gmail.com>
2024-01-01 13:20:17 +0530

    check connection in atlas

--- api/index.js
+++ api/index.js
@@ -5,8 +5,17 @@ dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
-mongoose.connect("mongodb+srv://admin-vigas:welcome123@cluster0.yieowhx.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-vigas:welcome123@cluster0.yieowhx.mongodb.net/User?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then( () => {
+    console.log('Connected to database ')
+})
+.catch( (err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+});
 
 app.listen(port, ()=>{
     console.log("Server is running on port 3000!");
+});
+
+app.get('/test', (req, res)=>{
+    res.send("Hello World!");
 });
