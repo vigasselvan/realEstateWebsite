c22022df82d9325d57cbd122829dfaf1b3d28910
vigas <vigas54209@gmail.com>
2024-01-02 08:52:33 +0530

    added env file and imported!

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv';    
+import { fileURLToPath } from 'url';
+import path from 'path';  // Import the 'path' module
+
+// Get the directory path of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Specify the path to the .env file
+const envFilePath = path.resolve(__dirname, '../config.env');  // Adjust the path based on your project structure
+
+// Load the environment variables from the specified .env file
+dotenv.config({ path: envFilePath });
 
 const port = process.env.PORT;
 const app = express();
@@ -13,7 +24,7 @@ mongoose.connect("mongodb+srv://admin-vigas:welcome123@cluster0.yieowhx.mongodb.
 });
 
 app.listen(port, ()=>{
-    console.log("Server is running on port 3000!");
+    console.log("Server is running on port " + port + "!");
 });
 
 app.get('/test', (req, res)=>{
