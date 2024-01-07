df70e36591cedfebc07606a61926abd515ed1160
vigas <vigas54209@gmail.com>
2024-01-07 15:49:12 +0530

    sign in page functionality completed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/users.model.js"; 
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import {errorHandler} from '../utils/error.js';
 
 export const signUp = async(req, res, next) => {
     const {username, email, password} = req.body;
